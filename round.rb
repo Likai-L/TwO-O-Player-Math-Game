8f3350c595aae4cbd0a833fcafb0a8ee758af79b
Likai Liu <likailiu2@gmail.com>
2023-01-26 03:05:29 +0800

    Add a prompt; fix bugs: correct variable names and the "--" operator

--- round.rb
+++ round.rb
@@ -4,18 +4,19 @@ class Round
       @num1 = rand(20) + 1
       @num2 = rand(20) + 1
       @answer = @num1 + @num2
+      @prompt = "#{player.name}, what does #{num1} plus #{num2} equal?"
     end 
   
-    attr_reader :num1, :num2, :answer
+    attr_reader :num1, :num2, :answer, :prompt
 
   
     def check(answer)
       unless answer.to_i == @answer
-        @current_player.lives --
-        puts "#{current_player.name}, are you kidding me? NOOO! You just lost one life!"
+        @current_player.lives -= 1
+        puts "#{@current_player.name}, are you kidding me? NOOO! You just lost one life!"
         return
       end
-      puts "Thats is correct, #{current_player.name}!"
+      puts "Thats is correct, #{@current_player.name}!"
     end
 
   end 
